9ba452d3a33677ea8b5097fb2c573fe6b4958f8a
Thomas Dekiere <t.dekiere@gmail.com>
2018-06-02 21:20:26 +0200

    Use node module resolution and import a config file, also added watch script

---
+++ config-sample.ts
@@ -0,0 +1,5 @@
+import { IConfig } from './src/ts/interfaces/IConfig'
+
+export const config: IConfig = {
+    apiKey: "GOOGLE API KEY COMES HERE"
+}
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,3 @@
-console.log("test");
+import { config } from '../../config';
+
+alert("test 5");
